d48164ed1d1b8f1d07af3c7d95fef232a8a76109
Maiken <info@maikenweb.nl>
2014-09-10 15:49:27 +0200

    GeoScript

--- Opdrachten/les-2/geo.js
+++ Opdrachten/les-2/geo.js
@@ -1,2 +1,46 @@
 /* Barebones GEO script */
 
+// Namespace aanmaken
+var cmdAanGeo = cmdAanGeo || {};
+
+// Self invoking function
+(function() {
+
+	// Configuratie mogelijk
+	cmdAanGeo.config =  {};
+
+	// Alle andere modules initialiseren
+	cmdAanGeo.controller = {
+		init: function() {}
+	};
+
+	// Alle GPS functions
+	cmdAanGeo.gps = {
+		startInterval: function() {},
+		updatePosition: function() {},
+		setPosition: function() {},
+		checkLocations: function() {},
+		calculateDistance: function() {}
+	};
+
+	// Alle GOOGLE MAPS functions
+	cmdAanGeo.map = {
+		generate: function() {},
+		updatePosition: function() {}
+	};
+
+	// Alle DEBUG functions
+	cmdAanGeo.debug = {
+		errorHandler: function() {},
+		message: function() {},
+		setCustomDebugging: function() {}
+	};
+
+	// Alle HELPER functions
+	cmdAanGeo.helper = {
+		isNumber: function() {}
+	};
+
+}());
+
+// Verwijderen door Margo
